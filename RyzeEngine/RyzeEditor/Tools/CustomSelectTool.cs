948513ecd2ec3690a049ed22e330ced8066bdb3f
Юрий Виханов <yur_vih@mail.ru>
2024-03-19 18:13:31 +0100

    CustomSelectTool changes

--- RyzeEngine/RyzeEditor/Tools/CustomSelectTool.cs
+++ RyzeEngine/RyzeEditor/Tools/CustomSelectTool.cs
@@ -11,6 +11,8 @@ namespace RyzeEditor.Tools
     public class CustomSelectTool : ToolBase, IVisualElement
     {
         bool _controlKeyPressed;
+        GameObject _nearestToCameraObject;
+        int? _subMeshIndex;
 
         public CustomSelectTool(WorldMap world, Selection selection) : base(world, selection)
         {
@@ -30,57 +32,7 @@ namespace RyzeEditor.Tools
 
         public override bool OnMouseDown(object sender, MouseEventArgs mouseEventArgs)
         {
-            var result = false;
-
-            if (mouseEventArgs.Button == MouseButtons.Right)
-            {
-                return result;
-            }
-
-            if (_world == null)
-            {
-                return result;
-            }
-
-            if (!_controlKeyPressed)
-            {
-                _selection.Clear();
-            }
-
-            var ray = _world.Camera.GetPickRay(mouseEventArgs.X, mouseEventArgs.Y);
-
-            GameObject nearestToCameraObject = null;
-            float distance = float.MaxValue;
-            int subMeshIndex = 0;
-
-            var intersects = GetGeometryIntersections(ray);
-
-            foreach (var intersect in intersects)
-            {
-                var dist = (intersect.Point - _world.Camera.Position).Length();
-                if (dist < distance)
-                {
-                    distance = dist;
-                    subMeshIndex = intersect.SubMeshIndex;
-                    nearestToCameraObject = _world.Entities.OfType<GameObject>().Where(x => x.Id == intersect.GameObjectId).FirstOrDefault();
-                }
-            }
-
-            if (nearestToCameraObject != null)
-            {
-                if (_controlKeyPressed && _selection.Get().OfType<GameObject>().Contains(nearestToCameraObject))
-                {
-                    _selection.RemoveEntity(nearestToCameraObject.Id);
-                }
-                else
-                {
-                    _selection.AddEntity(nearestToCameraObject);
-                }
-
-                result = true;
-            }
-
-            return result;
+            return true;
         }
 
         public override bool OnKeyboardInput(object sender, KeyboardInputEventArgs arg)
@@ -98,6 +50,16 @@ namespace RyzeEditor.Tools
             return _controlKeyPressed;
         }
 
+        public override bool OnMouseUp(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (mouseEventArgs.Button == MouseButtons.Right)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         public override bool OnMouseMove(object sender, MouseEventArgs mouseEventArgs)
         {
             if (mouseEventArgs.Button == MouseButtons.Right)
@@ -105,14 +67,29 @@ namespace RyzeEditor.Tools
                 return false;
             }
 
+            var ray = _world.Camera.GetPickRay(mouseEventArgs.X, mouseEventArgs.Y);
 
+            float distance = float.MaxValue;
+
+            var intersects = GetGeometryIntersections(ray);
+
+            foreach (var intersect in intersects)
+            {
+                var dist = (intersect.Point - _world.Camera.Position).Length();
+
+                if (dist < distance)
+                {
+                    distance = dist;
+                    _subMeshIndex = intersect.SubMeshIndex;
+                    _nearestToCameraObject = _world.Entities.OfType<GameObject>().Where(x => x.Id == intersect.GameObjectId).FirstOrDefault();
+                }
+            }
 
             return true;
         }
 
         public void Render3d(IRenderer renderer, RenderMode mode)
         {
-
             foreach (var gameObject in _selection.Get().OfType<GameObject>())
             {
                 //Highlight submesh BoundBox
